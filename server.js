6a24f73cd4b6a10bfc7c3d8ace9c5ce16e4235f0
Jackson <104618912+jacksongana@users.noreply.github.com>
2024-11-08 11:07:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,12 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, req.url);
+const app = express();
+const PORT = 3000;
 
-  // Si c'est un fichier PHP
-  if (filePath.endsWith('.php')) {
-    const php = require('child_process').spawn('php-cgi', ['-f', filePath]);
+// Servir les fichiers statiques (JavaScript, CSS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
 
-    php.stdout.on('data', (data) => {
-      res.write(data);
-    });
-
-    php.stderr.on('data', (data) => {
-      res.write(`Erreur PHP: ${data}`);
-    });
-
-    php.on('close', () => {
-      res.end();
-    });
-  } else {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.write('Page non trouvée');
-        res.end();
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-      }
-    });
-  }
-});
-
-server.listen(3000, () => {
-  console.log('Serveur Node.js en écoute sur http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Serveur JavaScript en écoute sur http://localhost:${PORT}`);
 });
